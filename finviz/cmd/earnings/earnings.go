4fe3476d97694be4db3c495b11a0b33d7d230285
James Bury <19847770+d3an@users.noreply.github.com>
2022-01-13 22:25:58 -0500

    Added support for Earnings (#83)

--- finviz/cmd/earnings/earnings.go
+++ finviz/cmd/earnings/earnings.go
@@ -3,55 +3,52 @@
 // Use of this source code is governed by a MIT-style license that
 // can be found in the LICENSE file for the project.
 
-package quote
+package calendar
 
 import (
 	"github.com/spf13/cobra"
 
-	"github.com/d3an/finviz/quote"
+	"github.com/d3an/finviz/earnings"
 	"github.com/d3an/finviz/utils"
 )
 
 var (
 	outputCSVArg  string
 	outputJSONArg string
-	tickerArgs    []string
 
 	// Cmd is the CLI subcommand for FinViz news
 	Cmd = &cobra.Command{
-		Use:     "quote",
-		Aliases: []string{"q", "quotes"},
-		Short:   "FinViz Quotes.",
-		Long:    "FinViz Quotes returns the quotes for tickers provided.",
+		Use:     "earnings",
+		Aliases: []string{"e"},
+		Short:   "Finviz Earnings.",
+		Long:    "Finviz Earnings returns the tickers with earnings releases left this week.",
 		Run: func(cmd *cobra.Command, args []string) {
 			var err error
 
-			client := quote.New(nil)
-			results, err := client.GetQuotes(tickerArgs)
+			client := earnings.New(nil)
+			df, err := client.GetEarnings()
 			if err != nil {
 				utils.Err(err)
 			}
 
 			if outputCSVArg != "" {
-				if err := utils.ExportCSV(results.Data, outputCSVArg); err != nil {
+				if err := utils.ExportCSV(df, outputCSVArg); err != nil {
 					utils.Err(err)
 				}
 			} else if outputJSONArg != "" {
-				if err := utils.ExportJSON(results.Data, outputJSONArg); err != nil {
+				if err := utils.ExportJSON(df, outputJSONArg); err != nil {
 					utils.Err(err)
 				}
 			} else {
-				utils.PrintFullDataFrame(results.Data)
+				utils.PrintFullDataFrame(df)
 			}
 		},
 	}
 )
 
 func init() {
-	// -v 1
 	// --output-csv data.csv
 	// --output-json data.json
-	Cmd.Flags().StringSliceVarP(&tickerArgs, "tickers", "t", nil, "AAPL,GS,amzn")
 	Cmd.Flags().StringVar(&outputCSVArg, "output-csv", "", "outputFileName.csv")
 	Cmd.Flags().StringVar(&outputJSONArg, "output-json", "", "outputFileName.json")
 }
